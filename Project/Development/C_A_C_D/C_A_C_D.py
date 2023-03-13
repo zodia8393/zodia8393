397cd245d9027d54381672c3638539d562f4d118
HJ <83521479+zodia8393@users.noreply.github.com>
2023-03-13 17:05:05 +0900

    Update C_A_C_D.py

--- Project/Development/C_A_C_D/C_A_C_D.py
+++ Project/Development/C_A_C_D/C_A_C_D.py
@@ -1,12 +1,22 @@
-#Guide Line For Chrome Auto Cache Deleter
-#Install the required packages: The first step is to install the necessary packages to automate the Chrome browser using Python. You'll need to install the selenium package, which is a Python library that provides a way to automate web browsers.
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-#Create a Python script: Once you have installed the required packages, the next step is to create a Python script that will automate the process of clearing the Chrome browser cache. You can use the webdriver class from the selenium package to create an instance of the Chrome browser, and then use the get method to navigate to the Chrome settings page where you can clear the cache.
+chrome_options = Options()
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
-#Find the appropriate XPaths: You'll need to find the appropriate XPaths that correspond to the Chrome settings page elements that you want to interact with using Python. This can be done by inspecting the page source and using the Chrome DevTools to find the XPaths of the elements you need to interact with.
+browser = webdriver.Chrome(options=chrome_options)
+browser.get("chrome://settings/clearBrowserData")
 
-#Use the find_element_by_xpath method: You can use the find_element_by_xpath method from the webdriver class to select the elements you want to interact with based on their XPaths. You can then use the click method to simulate a mouse click on the element, or the send_keys method to enter text into a form field.
+cache_xpath = '//*[@id="clearBrowsingDataConfirm"]/div/div/div[2]/div[1]/div[1]/label'
+cache_checkbox = browser.find_element_by_xpath(cache_xpath)
+cache_checkbox.click()
 
-#Automate the clearing of the cache: To automate the process of clearing the Chrome browser cache, you can use the appropriate XPaths to select the cache option and then click the "Clear Data" button.
+button_xpath = '//*[@class="md-button md-button--secondary settings-clear-data-dialog-confirm-button"]'
+button = browser.find_element_by_xpath(button_xpath)
+button.click()
 
-#Schedule the script to run at startup: Finally, you can schedule the Python script to run automatically when you turn on your computer by adding it to your startup folder or creating a task in the Task Scheduler. The exact steps will depend on your operating system.
+browser.close()
